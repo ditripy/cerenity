cc6031617abe744ddaaefd5f5d66940e78b0c29b
ditripy <robel.trinitymae@gmail.com>
2026-01-31 17:13:06 -0500

    landing page updates

--- catfolio/src/App.js
+++ catfolio/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function MainApp() {
   return (
-    <div>
+    <div className="main-app">
       <h1>Catfolio 🐱</h1>
       <p>Collect cats. Learn money. Avoid scams.</p>
     </div>
   );
+}
+
+function Landing({ onStart }) {
+  return (
+    <div className="landing">
+      <div className="title-container">
+        <img src="/catfolio.png" alt="Catfolio Logo" className="animated-logo" />
+        <img 
+          src="/fish_start.png" 
+          alt="Start Game" 
+          className="start-button-img" 
+          onClick={onStart}
+          style={{cursor: 'pointer'}}
+        />
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const [started, setStarted] = useState(false);
+
+  return started ? <MainApp /> : <Landing onStart={() => setStarted(true)} />;
 }
 
 export default App;
